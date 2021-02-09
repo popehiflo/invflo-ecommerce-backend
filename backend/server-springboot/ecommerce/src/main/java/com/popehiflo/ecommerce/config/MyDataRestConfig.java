bbc394e8cb361652c4c2a3d098339289f310aca8
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-08 21:37:42 -0500

    Deshabilita metodos HTTP que no son de lectura

---
+++ backend/server-springboot/ecommerce/src/main/java/com/popehiflo/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,33 @@
+package com.popehiflo.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+import com.popehiflo.ecommerce.entity.Product;
+import com.popehiflo.ecommerce.entity.ProductCategory;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		
+		HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+		
+		// Disable HTTP methods for Product: POST , PUT and DELETE
+		config.getExposureConfiguration()
+			.forDomainType(Product.class)
+			.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+			.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+		// Disable HTTP methods for ProductCategory: POST , PUT and DELETE
+				config.getExposureConfiguration()
+					.forDomainType(ProductCategory.class)
+					.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+					.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+	}
+	
+}
