ff82a690a27460fc433c728761ff7116d2720695
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-08 21:38:20 -0500

    Describe funcion del RepositoryRestResource

--- backend/server-springboot/ecommerce/src/main/java/com/popehiflo/ecommerce/dao/ProductCategoryRepository.java
+++ backend/server-springboot/ecommerce/src/main/java/com/popehiflo/ecommerce/dao/ProductCategoryRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.popehiflo.ecommerce.entity.ProductCategory;
-
+//	Modifica el path por defecto que nos ofrece
 // 		productCategory -> Name of JSON entry
 // 		/product-category 	-> This will be the actual reference
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
