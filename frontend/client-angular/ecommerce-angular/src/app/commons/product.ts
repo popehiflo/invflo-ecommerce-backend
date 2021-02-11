7ec575a6ab2de5ea6a15335b2d76fee021add530
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-11 17:01:34 -0500

    Se crea clase Productos para los productos

---
+++ frontend/client-angular/ecommerce-angular/src/app/commons/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+
+  // Propiedades segun el JSON recibido de la API
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  unitsInStock: number;
+  dateCreated: Date;
+  lastUpdated: Date;
+  categoryId: string;
+
+}
