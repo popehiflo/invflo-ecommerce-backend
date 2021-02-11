2d5b5d2ca33ca0dec7a10d45215064755ee34a7b
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-11 16:53:34 -0500

    Se crea componente 'product-list'

--- frontend/client-angular/ecommerce-angular/src/app/app.module.ts
+++ frontend/client-angular/ecommerce-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
