3596bf8c8f4ba832b8b8f0395277af6f562bfdb5
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-11 17:12:52 -0500

    Se crea service que lista productos (por ahora)

---
+++ frontend/client-angular/ecommerce-angular/src/app/services/product.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../commons/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  // Returns an Observable
+  /* Map the JSON data from Spring Data REST to Product array */
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+
+}
+
+// interface para mapear o desenvolver el JSON _embedded
+interface GetResponse{
+  _embedded: {
+    products: Product[];
+  }
+}
