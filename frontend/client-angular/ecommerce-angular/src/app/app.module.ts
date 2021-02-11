9dba72e59f08694b3e537941ffeed18656557122
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-11 17:07:32 -0500

    Import HttpClientModule y habilita ProductService

--- frontend/client-angular/ecommerce-angular/src/app/app.module.ts
+++ frontend/client-angular/ecommerce-angular/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,12 @@ import { ProductListComponent } from './components/product-list/product-list.com
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
