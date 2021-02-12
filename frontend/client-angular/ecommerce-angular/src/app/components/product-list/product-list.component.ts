72a1f67d56b397b6c0607f79836710f98abeb2b0
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-11 21:10:40 -0500

    Se agrega template para mostrar productos en tabla

--- frontend/client-angular/ecommerce-angular/src/app/components/product-list/product-list.component.ts
+++ frontend/client-angular/ecommerce-angular/src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,8 @@ import { Product } from '../../commons/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  // templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
