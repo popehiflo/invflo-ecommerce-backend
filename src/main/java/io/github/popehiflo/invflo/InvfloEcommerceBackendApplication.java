425f5601f0147c65be468416fabae90156593b9c
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2024-11-03 10:22:38 -0500

    feat: initialize project (backend) with spring initializr

---
+++ src/main/java/io/github/popehiflo/invflo/InvfloEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.popehiflo.invflo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvfloEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvfloEcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/popehiflo/invflo/InvfloEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.popehiflo.invflo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvfloEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
